316a1839d8c6a8839cee886d402fa555091d5d43
Tomasz Skora <tskora93@gmail.com>
2021-03-23 14:16:31 +0100

    _

--- pyBD/box.py
+++ pyBD/box.py
@@ -37,7 +37,7 @@ class Box():
 		self.mobile_beads = [ b for b in self.beads if b.mobile ]
 		self.mobile_bead_indices = [ i for i, b in enumerate(self.beads) if b.mobile ]
 		self.immobile_bead_indices = list( set([i for i in range(len(self.beads))]) - set(self.mobile_bead_indices) )
-		
+				
 		self.box_length = self.inp["box_length"]
 		self.T = self.inp["T"]
 		self.viscosity = self.inp["viscosity"]
