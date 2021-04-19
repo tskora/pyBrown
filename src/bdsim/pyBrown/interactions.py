e527e81ca17ae1eb4d36b1c2aabe5440979d9478
Tomasz Skora <tskora93@gmail.com>
2021-04-20 00:20:46 +0200

    interactions stub and planning

---
+++ src/bdsim/pyBrown/interactions.py
@@ -0,0 +1,36 @@
+# pyBrown is a Brownian and Stokesian dynamics simulation tool
+# Copyright (C) 2021  Tomasz Skora (tskora@ichf.edu.pl)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses.
+
+#-------------------------------------------------------------------------------
+
+class Interactions():
+
+	def __init__(self, beads, force_vector, force_function, force_parameters):
+
+		self.F = 
+
+		# **kwargs as a method of giving arguments to forces
+
+		
+
+	def compute_forces(self, beads, pointers):
+
+		pass
+
+	def compute_energy(self, beads, pointers):
+
+		pass
+
