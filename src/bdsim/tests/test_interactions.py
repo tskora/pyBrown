a1a17987e05b9b6e9f001f5f08245814b5afa7c9
Tomasz Skora <tskora93@gmail.com>
2021-04-20 23:39:53 +0200

    more tests

--- src/bdsim/tests/test_interactions.py
+++ src/bdsim/tests/test_interactions.py
@@ -31,93 +31,104 @@ class TestInteractions(unittest.TestCase):
 
 	def setUp(self):
 
-		pass
+		self.alpha = 4.0
+
+		self.epsilon = 1.0
+
+		self.d = 2.0
+
+		self.a = 1.0
+
+		self.b1 = Bead([0.0, 0.0, 0.0], self.a, epsilon_LJ = self.epsilon)
+
+		self.b2 = Bead([0.0, 0.0, self.d], self.a, epsilon_LJ = self.epsilon)
+
+		self.beads = [ self.b1, self.b2 ]
+
+		self.rij = np.zeros((len(self.beads), len(self.beads), 3))
+
+		for i in range(1, len(self.beads)):
+			for j in range(0, i):
+				self.rij[i][j] = pointer(self.beads[i], self.beads[j])
+				self.rij[j][i] = -self.rij[i][j]
 
 	#---------------------------------------------------------------------------
 
 	def test_force_and_energy_of_LJ_6_particles_in_sigma(self):
 
-		alpha = 4.0
-
-		epsilon = 1.0
-
-		d = 2.0
-
-		b1 = Bead([0.0, 0.0, 0.0], 1.0, epsilon_LJ = epsilon)
-
-		b2 = Bead([0.0, 0.0, d], 1.0, epsilon_LJ = epsilon)
-
-		beads = [ b1, b2 ]
-
-		rij = np.zeros((len(beads), len(beads), 3))
-
-		for i in range(1, len(beads)):
-			for j in range(0, i):
-				rij[i][j] = pointer(beads[i], beads[j])
-				rij[j][i] = -rij[i][j]
-
-		i = Interactions(LJ_6_attractive_force, LJ_6_attractive_energy, {"alpha": alpha})
-
 		F = np.zeros(6)
 
 		E = 0.0
 
-		E = i.compute_forces_and_energy([b1, b2], rij, F, E)
+		i = Interactions(LJ_6_attractive_force, LJ_6_attractive_energy, {"alpha": self.alpha})
 
-		self.assertEqual(E, -alpha*epsilon)
+		E = i.compute_forces_and_energy(self.beads, self.rij, F, E)
 
-		self.assertSequenceEqual(list(F), [0.0, 0.0, 6.0*alpha*epsilon/d, 0.0, 0.0, -6.0*alpha*epsilon/d])
+		self.assertEqual(E, -self.alpha*self.epsilon)
+
+		self.assertSequenceEqual(list(F), [0.0, 0.0, 6.0*self.alpha*self.epsilon/self.d, 0.0, 0.0, -6.0*self.alpha*self.epsilon/self.d])
 
 	#---------------------------------------------------------------------------
 
 	def test_force_and_energy_of_LJ_12_particles_in_sigma(self):
 
-		alpha = 4.0
-
-		epsilon = 1.0
-
-		d = 2.0
-
-		b1 = Bead([0.0, 0.0, 0.0], 1.0, epsilon_LJ = epsilon)
-
-		b2 = Bead([0.0, 0.0, d], 1.0, epsilon_LJ = epsilon)
-
-		beads = [ b1, b2 ]
-
-		rij = np.zeros((len(beads), len(beads), 3))
-
-		for i in range(1, len(beads)):
-			for j in range(0, i):
-				rij[i][j] = pointer(beads[i], beads[j])
-				rij[j][i] = -rij[i][j]
-
-		i = Interactions(LJ_12_repulsive_force, LJ_12_repulsive_energy, {"alpha": alpha})
-
 		F = np.zeros(6)
 
 		E = 0.0
 
-		E = i.compute_forces_and_energy([b1, b2], rij, F, E)
+		i = Interactions(LJ_12_repulsive_force, LJ_12_repulsive_energy, {"alpha": self.alpha})
 
-		self.assertEqual(E, alpha*epsilon)
+		E = i.compute_forces_and_energy(self.beads, self.rij, F, E)
 
-		self.assertSequenceEqual(list(F), [0.0, 0.0, -12.0*alpha*epsilon/d, 0.0, 0.0, 12.0*alpha*epsilon/d])
+		self.assertEqual(E, self.alpha*self.epsilon)
+
+		self.assertSequenceEqual(list(F), [0.0, 0.0, -12.0*self.alpha*self.epsilon/self.d, 0.0, 0.0, 12.0*self.alpha*self.epsilon/self.d])
 
 	#---------------------------------------------------------------------------
 
 	def test_force_and_energy_of_LJ_6_12_particles_in_sigma(self):
 
-		alpha = 4.0
+		F = np.zeros(6)
 
-		epsilon = 1.0
+		E = 0.0
 
-		d = 2.0
+		i = Interactions(LJ_6_12_force, LJ_6_12_energy, {"alpha": self.alpha})
 
-		b1 = Bead([0.0, 0.0, 0.0], 1.0, epsilon_LJ = epsilon)
+		ia = Interactions(LJ_6_attractive_force, LJ_6_attractive_energy, {"alpha": self.alpha})
 
-		b2 = Bead([0.0, 0.0, d], 1.0, epsilon_LJ = epsilon)
+		ir = Interactions(LJ_12_repulsive_force, LJ_12_repulsive_energy, {"alpha": self.alpha})
 
-		beads = [ b1, b2 ]
+		Fa = np.zeros(6)
+
+		Fr = np.zeros(6)
+
+		Ea = Er = 0.0
+
+		E = i.compute_forces_and_energy(self.beads, self.rij, F, E)
+
+		Ea = ia.compute_forces_and_energy(self.beads, self.rij, Fa, Ea)
+
+		Er = ir.compute_forces_and_energy(self.beads, self.rij, Fr, Er)
+
+		self.assertEqual(Ea + Er, 0.0)
+
+		self.assertEqual(E, 0.0)
+
+		self.assertSequenceEqual(list(F), [0.0, 0.0, -6.0*self.alpha*self.epsilon/self.d, 0.0, 0.0, 6.0*self.alpha*self.epsilon/self.d])
+
+		self.assertSequenceEqual(list(Fa + Fr), [0.0, 0.0, -6.0*self.alpha*self.epsilon/self.d, 0.0, 0.0, 6.0*self.alpha*self.epsilon/self.d])
+
+	#---------------------------------------------------------------------------
+
+	def test_force_and_energy_of_LJ_6_12_particles_at_minimum(self):
+
+		F = np.zeros(6)
+
+		E = 0.0
+
+		b3 = Bead([2*2.0**(1/6), 0.0, 0.0], 1.0, epsilon_LJ = self.epsilon)
+
+		beads = [self.b1, b3]
 
 		rij = np.zeros((len(beads), len(beads), 3))
 
@@ -126,33 +137,81 @@ class TestInteractions(unittest.TestCase):
 				rij[i][j] = pointer(beads[i], beads[j])
 				rij[j][i] = -rij[i][j]
 
-		i = Interactions(LJ_6_12_force, LJ_6_12_energy, {"alpha": alpha})
+		i = Interactions(LJ_6_12_force, LJ_6_12_energy, {"alpha": self.alpha})
 
-		ia = Interactions(LJ_6_attractive_force, LJ_6_attractive_energy, {"alpha": alpha})
+		E = i.compute_forces_and_energy(beads, rij, F, E)
 
-		ir = Interactions(LJ_12_repulsive_force, LJ_12_repulsive_energy, {"alpha": alpha})
+		self.assertEqual(E, -self.epsilon)
 
-		F = np.zeros(6)
+		for j in range(6):
 
-		Fa = np.zeros(6)
+			self.assertAlmostEqual(F[j], 0.0, places = 7)
 
-		Fr = np.zeros(6)
+	#---------------------------------------------------------------------------
 
-		E = Ea = Er = 0.0
+	def test_force_and_energy_of_LJ_6_12_particles_on_stable_equilateral_triangle(self):
 
-		E = i.compute_forces_and_energy([b1, b2], rij, F, E)
+		F = np.zeros(9)
 
-		Ea = ia.compute_forces_and_energy([b1, b2], rij, Fa, Ea)
+		E = 0.0
 
-		Er = ir.compute_forces_and_energy([b1, b2], rij, Fr, Er)
+		A = 2*2.0**(1/6) / ( np.cos(5*np.pi/6) - np.cos(np.pi/6) )
 
-		self.assertEqual(Ea + Er, 0.0)
+		bead4 = Bead([A*np.cos(np.pi/6), A*np.sin(np.pi/6), 0.0], 1.0, epsilon_LJ = 1.0)
 
-		self.assertEqual(E, 0.0)
+		bead5 = Bead([A*np.cos(5*np.pi/6), A*np.sin(5*np.pi/6), 0.0], 1.0, epsilon_LJ = 1.0)
 
-		self.assertSequenceEqual(list(F), [0.0, 0.0, -6.0*alpha*epsilon/d, 0.0, 0.0, 6.0*alpha*epsilon/d])
+		bead6 = Bead([A*np.cos(3*np.pi/2), A*np.sin(3*np.pi/2), 0.0], 1.0, epsilon_LJ = 1.0)
 
-		self.assertSequenceEqual(list(Fa + Fr), [0.0, 0.0, -6.0*alpha*epsilon/d, 0.0, 0.0, 6.0*alpha*epsilon/d])
+		beads = [ bead4, bead5, bead6 ]
+
+		rij = np.zeros((len(beads), len(beads), 3))
+
+		for i in range(1, len(beads)):
+			for j in range(0, i):
+				rij[i][j] = pointer(beads[i], beads[j])
+				rij[j][i] = -rij[i][j]
+
+		i = Interactions(LJ_6_12_force, LJ_6_12_energy, {"alpha": self.alpha})
+
+		E = i.compute_forces_and_energy(beads, rij, F, E)
+
+		self.assertEqual(E, -3*self.epsilon)
+
+		for j in range(9):
+
+			self.assertAlmostEqual(F[j], 0.0, places = 7)
+
+	#---------------------------------------------------------------------------
+
+	def test_energy_of_LJ_6_12_particles_on_unstable_equilateral_triangle(self):
+
+		F = np.zeros(9)
+
+		E = 0.0
+
+		A = 2 / ( np.cos(5*np.pi/6) - np.cos(np.pi/6) )
+
+		bead4 = Bead([A*np.cos(np.pi/6), A*np.sin(np.pi/6), 0.0], 1.0, epsilon_LJ = 1.0)
+
+		bead5 = Bead([A*np.cos(5*np.pi/6), A*np.sin(5*np.pi/6), 0.0], 1.0, epsilon_LJ = 1.0)
+
+		bead6 = Bead([A*np.cos(3*np.pi/2), A*np.sin(3*np.pi/2), 0.0], 1.0, epsilon_LJ = 1.0)
+
+		beads = [ bead4, bead5, bead6 ]
+
+		rij = np.zeros((len(beads), len(beads), 3))
+
+		for i in range(1, len(beads)):
+			for j in range(0, i):
+				rij[i][j] = pointer(beads[i], beads[j])
+				rij[j][i] = -rij[i][j]
+
+		i = Interactions(LJ_6_12_force, LJ_6_12_energy, {"alpha": self.alpha})
+
+		E = i.compute_forces_and_energy(beads, rij, F, E)
+
+		self.assertAlmostEqual(E, 0.0, places = 7)
 
 #-------------------------------------------------------------------------------
 
