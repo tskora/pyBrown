b76bbc0ea214f89ab2bdf74976ab87d63a7252e1
Tomasz Skora <tskora93@gmail.com>
2021-04-20 21:22:35 +0200

    interactions module added with LJ and basic tests

--- src/bdsim/pyBrown/interactions.py
+++ src/bdsim/pyBrown/interactions.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see https://www.gnu.org/licenses.
 
+import math
+
 #-------------------------------------------------------------------------------
 
 class Interactions():
@@ -24,35 +26,154 @@ class Interactions():
 
 		self.energy = energy_function
 
-		self.auxiliary_forca_parameters = auxiliary_force_parameters
+		self.auxiliary_force_parameters = auxiliary_force_parameters
 
-		# **kwargs as a method of giving arguments to forces
+	#-------------------------------------------------------------------------------
 
-	def compute_forces_and_energy(self, beads, pointers, F, E):
+	def compute_forces_and_energy(self, beads, pointers, F, E = None):
 
-		for i, beadi in enumerate(1, beads):
+		for i in range(1, len(beads)):
 
-			beadi_params = ...
+			beadi = beads[i]
 
-			for j in range(i-1):
+			for j in range(i):
 
 				beadj = beads[j]
 
 				pointerij = pointers[i][j]
 
-				beadj_params = ...
+				f = self._compute_force(beadi, beadj, pointerij, self.auxiliary_force_parameters)
 
-				F[3*i:3*(i+1)] += self._compute_force(beadi, beadj, pointerij, **auxiliary_force_parameters)
+				F[3*i:3*(i+1)] += f
 
-				F[3*j:3*(j+1)] -= self._compute_force(beadi, beadj, pointerij, **auxiliary_force_parameters)
+				F[3*j:3*(j+1)] -= f
 
-				E += self._compute_pair_energy(beadi, beadj, pointerij, **auxiliary_force_parameters)
+				if E is not None: E += self._compute_pair_energy(beadi, beadj, pointerij, self.auxiliary_force_parameters)
 
-	def _compute_force(self, bead1, bead2, pointer, **kwargs):
+		return E
 
-		return self.force(bead1, bead2, pointer, ...)
+	#-------------------------------------------------------------------------------
 
-	def _compute_pair_energy(self, bead1, bead2, pointer, **kwargs):
+	def _compute_force(self, bead1, bead2, pointer, auxiliary_force_parameters):
 
-		return self.energy(bead1, bead2, pointer, ...)
+		return self.force(bead1, bead2, pointer, **auxiliary_force_parameters)
+
+	#-------------------------------------------------------------------------------
+
+	def _compute_pair_energy(self, bead1, bead2, pointer, auxiliary_force_parameters):
+
+		return self.energy(bead1, bead2, pointer, **auxiliary_force_parameters)
+
+#-------------------------------------------------------------------------------
+
+def LJ_6_attractive_force(bead1, bead2, pointer, alpha):
+
+	sigma = bead1.hard_core_radius + bead2.hard_core_radius
+
+	epsilon = math.sqrt( bead1.epsilon_LJ * bead2.epsilon_LJ )
+
+	dist2 = pointer[0]*pointer[0] + pointer[1]*pointer[1] + pointer[2]*pointer[2]
+
+	dist = math.sqrt(dist2)
+
+	s2 = sigma * sigma / dist2
+
+	s6 = s2 * s2 * s2
+
+	versor = pointer / dist
+
+	return 6.0*alpha*epsilon*s6/dist*versor
+
+#-------------------------------------------------------------------------------
+
+def LJ_6_attractive_energy(bead1, bead2, pointer, alpha):
+
+	sigma = bead1.hard_core_radius + bead2.hard_core_radius
+
+	epsilon = math.sqrt( bead1.epsilon_LJ * bead2.epsilon_LJ )
+
+	dist2 = pointer[0]*pointer[0] + pointer[1]*pointer[1] + pointer[2]*pointer[2]
+
+	s2 = sigma * sigma / dist2
+
+	s6 = s2 * s2 * s2
+
+	return -alpha*epsilon*s6
+
+#-------------------------------------------------------------------------------
+
+def LJ_12_repulsive_force(bead1, bead2, pointer, alpha):
+
+	sigma = bead1.hard_core_radius + bead2.hard_core_radius
+
+	epsilon = math.sqrt( bead1.epsilon_LJ * bead2.epsilon_LJ )
+
+	dist2 = pointer[0]*pointer[0] + pointer[1]*pointer[1] + pointer[2]*pointer[2]
+
+	dist = math.sqrt(dist2)
+
+	s2 = sigma * sigma / dist2
+
+	s12 = s2 * s2 * s2 * s2 * s2 * s2
+
+	versor = pointer / dist
+
+	return -12.0*alpha*epsilon*s12/dist*versor
+
+#-------------------------------------------------------------------------------
+
+def LJ_12_repulsive_energy(bead1, bead2, pointer, alpha):
+
+	sigma = bead1.hard_core_radius + bead2.hard_core_radius
+
+	epsilon = math.sqrt( bead1.epsilon_LJ * bead2.epsilon_LJ )
+
+	dist2 = pointer[0]*pointer[0] + pointer[1]*pointer[1] + pointer[2]*pointer[2]
+
+	s2 = sigma * sigma / dist2
+
+	s12 = s2 * s2 * s2 * s2 * s2 * s2
+
+	return alpha*epsilon*s12
+
+#-------------------------------------------------------------------------------
+
+def LJ_6_12_energy(bead1, bead2, pointer, alpha):
+
+	sigma = bead1.hard_core_radius + bead2.hard_core_radius
+
+	epsilon = math.sqrt( bead1.epsilon_LJ * bead2.epsilon_LJ )
+
+	dist2 = pointer[0]*pointer[0] + pointer[1]*pointer[1] + pointer[2]*pointer[2]
+
+	s2 = sigma * sigma / dist2
+
+	s6 = s2 * s2 * s2
+
+	s12 = s6 * s6
+
+	return alpha*epsilon*(s12 - s6)
+
+#-------------------------------------------------------------------------------
+
+def LJ_6_12_force(bead1, bead2, pointer, alpha):
+
+	sigma = bead1.hard_core_radius + bead2.hard_core_radius
+
+	epsilon = math.sqrt( bead1.epsilon_LJ * bead2.epsilon_LJ )
+
+	dist2 = pointer[0]*pointer[0] + pointer[1]*pointer[1] + pointer[2]*pointer[2]
+
+	dist = math.sqrt(dist2)
+
+	s2 = sigma * sigma / dist2
+
+	s6 = s2 * s2 * s2
+
+	s12 = s6 * s6
+
+	versor = pointer / dist
+
+	return alpha*epsilon/dist*(6*s6-12*s12)*versor
+
 
--- src/bdsim/tests/test_interactions.py
+++ src/bdsim/tests/test_interactions.py
@@ -14,9 +14,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see https://www.gnu.org/licenses.
 
+import numpy as np
+import os
+import sys
+sys.path.insert(0, os.path.abspath( os.path.join(os.path.dirname(__file__), '..') ))
 import unittest
 
-from pyBrown.interactions import Interactions
+from pyBrown.bead import Bead, pointer
+from pyBrown.interactions import Interactions, LJ_6_attractive_energy, LJ_6_attractive_force,\
+								 LJ_12_repulsive_energy, LJ_12_repulsive_force,\
+								 LJ_6_12_energy, LJ_6_12_force
 
 #-------------------------------------------------------------------------------
 
@@ -28,10 +35,124 @@ class TestInteractions(unittest.TestCase):
 
 	#---------------------------------------------------------------------------
 
-	def test_example(self):
+	def test_force_and_energy_of_LJ_6_particles_in_sigma(self):
 
-		pass
+		alpha = 4.0
 
+		epsilon = 1.0
+
+		d = 2.0
+
+		b1 = Bead([0.0, 0.0, 0.0], 1.0, epsilon_LJ = epsilon)
+
+		b2 = Bead([0.0, 0.0, d], 1.0, epsilon_LJ = epsilon)
+
+		beads = [ b1, b2 ]
+
+		rij = np.zeros((len(beads), len(beads), 3))
+
+		for i in range(1, len(beads)):
+			for j in range(0, i):
+				rij[i][j] = pointer(beads[i], beads[j])
+				rij[j][i] = -rij[i][j]
+
+		i = Interactions(LJ_6_attractive_force, LJ_6_attractive_energy, {"alpha": alpha})
+
+		F = np.zeros(6)
+
+		E = 0.0
+
+		E = i.compute_forces_and_energy([b1, b2], rij, F, E)
+
+		self.assertEqual(E, -alpha*epsilon)
+
+		self.assertSequenceEqual(list(F), [0.0, 0.0, 6.0*alpha*epsilon/d, 0.0, 0.0, -6.0*alpha*epsilon/d])
+
+	#---------------------------------------------------------------------------
+
+	def test_force_and_energy_of_LJ_12_particles_in_sigma(self):
+
+		alpha = 4.0
+
+		epsilon = 1.0
+
+		d = 2.0
+
+		b1 = Bead([0.0, 0.0, 0.0], 1.0, epsilon_LJ = epsilon)
+
+		b2 = Bead([0.0, 0.0, d], 1.0, epsilon_LJ = epsilon)
+
+		beads = [ b1, b2 ]
+
+		rij = np.zeros((len(beads), len(beads), 3))
+
+		for i in range(1, len(beads)):
+			for j in range(0, i):
+				rij[i][j] = pointer(beads[i], beads[j])
+				rij[j][i] = -rij[i][j]
+
+		i = Interactions(LJ_12_repulsive_force, LJ_12_repulsive_energy, {"alpha": alpha})
+
+		F = np.zeros(6)
+
+		E = 0.0
+
+		E = i.compute_forces_and_energy([b1, b2], rij, F, E)
+
+		self.assertEqual(E, alpha*epsilon)
+
+		self.assertSequenceEqual(list(F), [0.0, 0.0, -12.0*alpha*epsilon/d, 0.0, 0.0, 12.0*alpha*epsilon/d])
+
+	#---------------------------------------------------------------------------
+
+	def test_force_and_energy_of_LJ_6_12_particles_in_sigma(self):
+
+		alpha = 4.0
+
+		epsilon = 1.0
+
+		d = 2.0
+
+		b1 = Bead([0.0, 0.0, 0.0], 1.0, epsilon_LJ = epsilon)
+
+		b2 = Bead([0.0, 0.0, d], 1.0, epsilon_LJ = epsilon)
+
+		beads = [ b1, b2 ]
+
+		rij = np.zeros((len(beads), len(beads), 3))
+
+		for i in range(1, len(beads)):
+			for j in range(0, i):
+				rij[i][j] = pointer(beads[i], beads[j])
+				rij[j][i] = -rij[i][j]
+
+		i = Interactions(LJ_6_12_force, LJ_6_12_energy, {"alpha": alpha})
+
+		ia = Interactions(LJ_6_attractive_force, LJ_6_attractive_energy, {"alpha": alpha})
+
+		ir = Interactions(LJ_12_repulsive_force, LJ_12_repulsive_energy, {"alpha": alpha})
+
+		F = np.zeros(6)
+
+		Fa = np.zeros(6)
+
+		Fr = np.zeros(6)
+
+		E = Ea = Er = 0.0
+
+		E = i.compute_forces_and_energy([b1, b2], rij, F, E)
+
+		Ea = ia.compute_forces_and_energy([b1, b2], rij, Fa, Ea)
+
+		Er = ir.compute_forces_and_energy([b1, b2], rij, Fr, Er)
+
+		self.assertEqual(Ea + Er, 0.0)
+
+		self.assertEqual(E, 0.0)
+
+		self.assertSequenceEqual(list(F), [0.0, 0.0, -6.0*alpha*epsilon/d, 0.0, 0.0, 6.0*alpha*epsilon/d])
+
+		self.assertSequenceEqual(list(Fa + Fr), [0.0, 0.0, -6.0*alpha*epsilon/d, 0.0, 0.0, 6.0*alpha*epsilon/d])
 
 #-------------------------------------------------------------------------------
 
